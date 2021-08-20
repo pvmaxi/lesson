64a26a0119cdb32b8db5fcc256b8f028b17877ab
maxmo <maxmonk00@gmail.com>
2021-08-20 14:23:57 +0300

    activity

--- app/src/main/java/ru/startandroid/develop/myapplication/InterestsActivity.kt
+++ app/src/main/java/ru/startandroid/develop/myapplication/InterestsActivity.kt
@@ -4,14 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import ru.startandroid.develop.myapplication.databinding.ActivityInterestsBinding
 
 class InterestsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInterestsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_interests)
+        binding = ActivityInterestsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<ImageView>(R.id.fz_logo).setOnClickListener{
+        binding.fzLogo.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+//        setContentView(R.layout.activity_interests)
+//
+//        findViewById<ImageView>(R.id.fz_logo).setOnClickListener{
+//            startActivity(Intent(this, RegisterActivity::class.java))
+//        }
     }
 }
